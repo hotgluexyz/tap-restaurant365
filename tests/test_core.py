e9eb17c47d78409bad4395d03b9c8601aede0de2
Lior Ben-David <liorbd@Outlook.com>
2026-05-27 10:28:57 -0400

    Revert "HGI-10383: migrate to hotglue-singer-sdk + filter bills by vendor (#30)" (#31)
    This reverts commit 0f23fb799f495ab6e784f75a342d56cd2632a997.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,23 +1,22 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 import datetime
-import pytest
 
-from hotglue_singer_sdk.testing import get_standard_tap_tests
+from singer_sdk.testing import get_tap_test_class
+
 from tap_restaurant365.tap import TapRestaurant365
 
 SAMPLE_CONFIG = {
     "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
-    "username": "test",
-    "password": "test",
-    "store_name": "test",
     # TODO: Initialize minimal tap config
 }
 
 
 # Run standard built-in tap tests from the SDK:
-@pytest.mark.parametrize("test_fn", get_standard_tap_tests(TapRestaurant365, SAMPLE_CONFIG)[:2])
-def test_tap(test_fn):
-    test_fn()
+TestTapRestaurant365 = get_tap_test_class(
+    tap_class=TapRestaurant365,
+    config=SAMPLE_CONFIG,
+)
+
 
 # TODO: Create additional tests as appropriate for your tap.
