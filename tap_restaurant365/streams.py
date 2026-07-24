415a0f975c781f5883ac5c807c9a1868981e38b0
Jacob Mazurkiewicz <jacobmazurkiewicz6616@gmail.com>
2026-07-24 08:58:11 -0400

    Changed PK to rowVersion for entity_deleted stream

--- tap_restaurant365/streams.py
+++ tap_restaurant365/streams.py
@@ -571,7 +571,7 @@ class EntityDeletedStream(Restaurant365Stream):
 
     name = "entity_deleted"
     path = "/EntityDeleted"
-    primary_keys = ["entityId"]
+    primary_keys = ["rowVersion"]
     replication_key = "deletedOn"
     schema = th.PropertiesList(
         th.Property("entityId", th.StringType),
